8d620394195069510bfe0e07fafa9f2a497c0ff2
Surendra2611 <2611surendrayadav2712@gmail.com>
2023-03-17 12:07:27 +0530

    java project

---
+++ src/main/java/com/example/watchlistbasicSpringProject/WatchlistBasicSpringProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.watchlistbasicSpringProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchlistBasicSpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatchlistBasicSpringProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/watchlistbasicSpringProject/WatchlistBasicSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.watchlistbasicSpringProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchlistBasicSpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
